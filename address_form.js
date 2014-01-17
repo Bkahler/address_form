c7a059a5f1db6cb4e8ddf86d42327a231a562d13
Bryan K <bkahler3@gmail.com>
2014-01-16 19:09:01 -0800

    finishes inline validation

--- address_form.js
+++ address_form.js
@@ -2,12 +2,11 @@ $(function(){
   validate_submit();
   validate_inline();
 
+  //inline validation function to test if appropriate entry has been entered in each field before submit
   function validate_inline(){
-
     // Start of inline test for streetNumber
     $('#streetNumber').blur(function(event){
       if ($('#streetNumber').val().match(/^\d+\s[a-zA-Z]+/) ){
-        streetNumberValid = true
         $('#streetNumber').removeClass("error")
         $('#street_error').css('visibility', 'hidden');
       }
@@ -16,18 +15,49 @@ $(function(){
         $('#street_error').css('visibility', 'visible');
       };
     });
-    //end
+    //end of inline test for street number
+
     //Start of inline test for city
     $('#city').blur(function(event){
-      if ($('#city').val() == "" ){
+      if ($('#city').val().match(/^[a-zA-Z]/)){
+        $('#city').removeClass("error");
+        $('#city_error').css('visibility', 'hidden');
+      }
+      else{
         $('#city').addClass("error")
         $('#city_error').css('visibility', 'visible');
       }
+    });
+    //end of inline test for city
+
+    //Start of inline test for State
+    $('#state').blur(function(event){
+
+      if($('#state').val() ==""){
+        $('#state').addClass('error');
+        $('#state_error').css('visibility', 'visible');
+      }
       else{
-        $('#city').removeClass("error");
-        $('#city_error').css('visibility', 'hidden');
+        $('#state').removeClass('error');
+        $('#state_error').css('visibility', 'hidden');
       };
     });
+    //end of inline test for state
+
+    //Start of inline test for zipcode
+    $('#zip').blur(function(event){
+
+      if($('#zip').val().match(/\d{5}/)){
+        $('#zip').removeClass('error');
+        $('#zip_error').css('visibility','hidden')
+      }
+      else{
+        $('#zip').addClass('error');
+        $('#zip_error').css('visibility','visible')
+      }
+
+    })
+    //end of zip test
 
 
 
