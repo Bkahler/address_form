21bed5edcb7cd90a67ba934f4131bc37fb3bc771
Bryan K <bkahler3@gmail.com>
2014-01-16 19:23:36 -0800

    form submittal works

--- validation.js
+++ validation.js
@@ -6,55 +6,24 @@ function validateInput(){
   var stateValid = false
   var zipValid = false
 
-  $('#errorMessage').empty();
 
       if ($('#streetNumber').val().match(/^\d+\s[a-zA-Z]+/) ){
         streetNumberValid = true
-        $('#streetNumber').removeClass("error")
-      }
-      else{
-        $('#streetNumber').addClass("error")
-        $('#errorMessage').append('<li>Street Number cannot be blank </li>')
       };
 
-      if($('#unit').val().length > 4){
-        $('#unit').addClass("error")
-        $('#errorMessage').append('<li> That does not appear to be a valid unit number</li>')
-      }
-      else{
-        unitValid = true
-        $('#unit').removeClass("error")
-      };
-
-      if($('#city').val() ==""){
-        $('#city').addClass("error")
-        $('#errorMessage').append('<li>City cannot be blank </li>')
-      }
-      else{
+      if ($('#city').val().match(/^[a-zA-Z]/)){
         cityValid = true
-        $('#city').removeClass("error")
       };
 
-      if($('#state').val() ==""){
-        $('#state').addClass("error")
-        $('#errorMessage').append('<li>Please Select a State</li>')
-      }
-      else{
+      if($('#state').val() !==""){
         stateValid = true
-        $('#state').removeClass("error")
       };
 
       if ($('#zip').val().match(/\d{5}/) ){
-        alert('zip is good to go!');
-        $('#zip').removeClass("error");
         zipValid = true
-      }
-      else{
-        $('#zip').addClass("error")
-        $('#errorMessage').append('<li>Please provide a valid Zip Code</li>')
       };
 
-      if (zipValid && stateValid && cityValid && unitValid && streetNumberValid == true){
+      if (zipValid && stateValid && cityValid && streetNumberValid == true){
         alert('your form is valid')
       }
       else{
@@ -66,5 +35,4 @@ function validateInput(){
           stateValid = false
           zipValid = false
       };
-
     };
